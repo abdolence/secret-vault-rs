e47fc5dd9d5e2c9dc7f92decd60d1036eec496c9
Abdulla Abdurakhmanov <me@abdolence.dev>
2023-01-27 12:01:41 +0100

    Fixed deprecated code

--- secret-value/src/base64_support.rs
+++ secret-value/src/base64_support.rs
@@ -1,18 +1,19 @@
 use crate::SecretValue;
 use zeroize::{Zeroize, Zeroizing};
+use base64::prelude::*;
 
 impl SecretValue {
     pub fn as_sensitive_base64_str(&self) -> Zeroizing<String> {
-        base64::encode(self.as_sensitive_bytes()).into()
+        BASE64_STANDARD.encode(self.as_sensitive_bytes()).into()
     }
 
     pub fn to_base64_str(&self) -> Self {
         Self::from(&self.as_sensitive_base64_str())
     }
 
-    pub fn from_base64_str(mut hex_string: String) -> Result<Self, base64::DecodeError> {
-        let result = Self::new(base64::decode(&hex_string)?);
-        hex_string.zeroize();
+    pub fn from_base64_str(mut base64_string: String) -> Result<Self, base64::DecodeError> {
+        let result = Self::new(BASE64_STANDARD.decode(&base64_string)?);
+        base64_string.zeroize();
         Ok(result)
     }
 }
@@ -31,15 +32,15 @@ mod test {
         #[test]
         fn test_base64_encode(secret_value in generate_secret_value()) {
             let base64_str = secret_value.as_sensitive_base64_str();
-            assert_eq!(secret_value.as_sensitive_bytes(), base64::decode(base64_str).unwrap());
+            assert_eq!(secret_value.as_sensitive_bytes(), BASE64_STANDARD.decode(base64_str).unwrap());
 
             let base64_value = secret_value.to_base64_str();
-            assert_eq!(secret_value.as_sensitive_bytes(), base64::decode(base64_value.as_sensitive_str()).unwrap());
+            assert_eq!(secret_value.as_sensitive_bytes(), BASE64_STANDARD.decode(base64_value.as_sensitive_str()).unwrap());
         }
 
         #[test]
         fn test_base64_decode(mock_str in ".*") {
-            let base64_str = base64::encode(mock_str.as_bytes());
+            let base64_str = BASE64_STANDARD.encode(mock_str.as_bytes());
             let secret_value = SecretValue::from_base64_str(base64_str).unwrap();
             assert_eq!(secret_value.as_sensitive_bytes(), mock_str.as_bytes());
         }
