0865fc631136e027b9e5adad8296016b06cb50f8
Abdulla Abdurakhmanov <me@abdolence.dev>
2024-04-13 23:13:14 +0200

    chrono_time_from_prost fixed deprecated code

--- secret-vault/src/prost_chrono.rs
+++ secret-vault/src/prost_chrono.rs
@@ -3,8 +3,7 @@ use chrono::prelude::*;
 pub(crate) fn chrono_time_from_prost(
     ts: gcloud_sdk::prost_types::Timestamp,
 ) -> Option<DateTime<Utc>> {
-    chrono::NaiveDateTime::from_timestamp_opt(ts.seconds, ts.nanos as u32)
-        .map(|dt| DateTime::<Utc>::from_naive_utc_and_offset(dt, Utc))
+    DateTime::<Utc>::from_timestamp(ts.seconds, ts.nanos as u32)
 }
 
 pub(crate) fn chrono_duration_from_prost(
